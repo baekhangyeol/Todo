38c7a55ca73e7b19d2a1ee5e38a0b49e556b9d92
hangyeol <snake011128@naver.com>
2024-03-17 02:29:30 +0900

    [#9]feat: RefreshToken 추가

---
+++ src/main/java/com/project/todo/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.project.todo.domain.auth.domain;
+
+import jakarta.persistence.Id;
+import java.util.Collection;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.security.core.GrantedAuthority;
+
+@RedisHash(value = "RE")
+@Getter
+@Builder
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    private String token;
+
+    @Indexed
+    private String email;
+
+    @TimeToLive
+    @Builder.Default
+    private Long expiration = 7L * 24L * 60L * 60L;
+
+    public Collection<? extends GrantedAuthority> authorities;
+}
