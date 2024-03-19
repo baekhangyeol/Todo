893b3a012281c0f946b8f4a90ced7bce1c752f73
hangyeol <snake011128@naver.com>
2024-03-19 22:33:43 +0900

    [#9]feat: Todo 도메인 추가

---
+++ src/main/java/com/project/todo/domain/todo/domain/Todo.java
@@ -0,0 +1,34 @@
+package com.project.todo.domain.todo.domain;
+
+import com.project.todo.domain.member.domain.Member;
+import com.project.todo.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private boolean isChecked;
+    private LocalDateTime dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
