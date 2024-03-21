3b40d827b72e20a82735416ae68191b6f385516f
hangyeol <snake011128@naver.com>
2024-03-21 11:17:25 +0900

    [#13]feat: api 태그 설정, jwt 인증 추가

--- src/main/java/com/project/todo/global/config/SwaggerConfig.java
+++ src/main/java/com/project/todo/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.project.todo.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,17 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme apiKey = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .in(SecurityScheme.In.HEADER)
+            .scheme("bearer")
+            .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer Token");
+
         return new OpenAPI()
-            .components(new Components())
+            .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+            .addSecurityItem(securityRequirement)
             .info(apiInfo());
     }
 
