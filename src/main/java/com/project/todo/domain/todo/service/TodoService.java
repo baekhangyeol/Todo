8ea13ea09c78fdb3209fba383db32cf6786ff729
hangyeol <snake011128@naver.com>
2024-03-23 00:42:16 +0900

    [#11]fix: 수정할 때 Member 검증 후 생성자가 아니면 오류 반환하게 수정

--- src/main/java/com/project/todo/domain/todo/service/TodoService.java
+++ src/main/java/com/project/todo/domain/todo/service/TodoService.java
@@ -7,6 +7,9 @@ import com.project.todo.domain.todo.dto.request.UpdateTodoRequest;
 import com.project.todo.domain.todo.dto.response.CreateTodoResponse;
 import com.project.todo.domain.todo.dto.response.UpdateTodoResponse;
 import com.project.todo.domain.todo.repository.TodoRepository;
+import com.project.todo.global.error.ErrorCode;
+import com.project.todo.global.error.exception.EmptyResultDataException;
+import com.project.todo.global.error.exception.UnauthorizedAccessException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,7 +30,8 @@ public class TodoService {
 
     @Transactional
     public UpdateTodoResponse updateTodo(Long id, UpdateTodoRequest request, Member member) {
-        Todo todo = todoRepository.findById(id).orElseThrow();
+        Todo todo = todoRepository.findById(id).orElseThrow(() -> new EmptyResultDataException(ErrorCode.TODO_NOT_FOUND));
+        if(todo.getMember() != member) throw new UnauthorizedAccessException(ErrorCode.TODO_NOT_ACCESS);
         todo.update(request);
         return UpdateTodoResponse.from(todo.getId());
     }
